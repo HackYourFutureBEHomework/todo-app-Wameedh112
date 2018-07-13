adf3647c7dc48b4d82e8b3a99a54b1dc04d1e3f4
Wameedh <38753468+Wameedh112@users.noreply.github.com>
2018-07-13 13:54:57 +0200

    Clearing completed items
    Step 4: (Optional) Todo app: Clearing completed items

--- js/todo.js
+++ js/todo.js
@@ -46,6 +46,17 @@ function update() {
         }     
       } 
 
+      //4.1 In our update function, get the list of completed items (hint: use filter to filter on items that are done).
+      // Count the number of items in that list. If it's bigger than zero, show the .clear-completed button, otherwise hide it.
+        const $completed = TODOS.filter(trueDone => trueDone.done);
+      if($completed.length > 0){
+        const $ClearCompleted = document.querySelector('.clear-completed');
+           $ClearCompleted.innerHTML= 'Clear completed';
+        }else {
+    const $ClearCompleted = document.querySelector('.clear-completed');
+    $ClearCompleted.innerHTML= ' ';
+      }
+
 // To remove the block of style if the array of items empty
       if (TODOS.length === 0) {
         document.querySelector('.main').style.display = 'none';
@@ -54,31 +65,43 @@ function update() {
         }
       }
        
-function onToggleTodo(id) {
-  const todo = TODOS.find(todo => todo.id === id);
-  // TODOS.find(function(todo) { return todo.id === id; });
-  todo.done = !todo.done;
-  update();
-}
+  function onToggleTodo(id) {
+    const todo = TODOS.find(todo => todo.id === id);
+    // TODOS.find(function(todo) { return todo.id === id; });
+    todo.done = !todo.done;
+    update();
+  }
 
 // Delete function
- function onDeleteItem(id){
-  TODOS = TODOS.filter($dItem => $dItem.id != id);
-   update();
- }
+  function onDeleteItem(id){
+    TODOS = TODOS.filter($dItem => $dItem.id != id);
+    update();
+  }
 
-function onNewTodo(e) {
-  const title = e.target.value;
-  console.log(title);
-  TODOS.push({
-    id: Date.now(),
-    title,
-    done: false
-  });
-  update();
-  e.target.value = "";
-}
+//4.3 In that function, use filter on the entire list of TODOS to remove completed items. 
+  function ClearCompleted () {
+    TODOS = TODOS.filter($cItem => !$cItem.done);
+    update();
+  }
+
+  function onNewTodo(e) {
+    const title = e.target.value;
+    console.log(title);
+    TODOS.push({
+      id: Date.now(),
+      title,
+      done: false
+    });
+    update();
+    e.target.value = "";
+  }
 
 // Select the new todo input field.
-const $newTodo = document.querySelector(".new-todo");
-$newTodo.addEventListener("change", onNewTodo);
+  const $newTodo = document.querySelector(".new-todo");
+  $newTodo.addEventListener("change", onNewTodo);
+
+//4.2 Add an event listener add the very end of our file on the .clear-completed button that reacts to a click event.
+// Make it call a function that you create.
+
+  const $clearCompleted = document.querySelector(".clear-completed");
+  $clearCompleted.addEventListener("click", ClearCompleted);
