55a978dfe952cc36a568a80680a0df9af35a5b42
Wameedh <38753468+Wameedh112@users.noreply.github.com>
2018-07-20 23:44:59 +0200

    Add the filters

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,5 @@
 let TODOS = [];
-
+let Filtersno="";
 function update() {
   const $todoList = document.querySelector(".todo-list");
   $todoList.innerHTML = "";
@@ -43,12 +43,13 @@ function update() {
        }          
         else {
           $Span.innerHTML = $unFinished.length + ' items left';
-        }     
-      } 
+        } 
+      }//End For.************** 
 
       //4.1 In our update function, get the list of completed items (hint: use filter to filter on items that are done).
       // Count the number of items in that list. If it's bigger than zero, show the .clear-completed button, otherwise hide it.
         const $completed = TODOS.filter(trueDone => trueDone.done);
+        
       if($completed.length > 0){
         const $ClearCompleted = document.querySelector('.clear-completed');
            $ClearCompleted.innerHTML= 'Clear completed';
@@ -57,13 +58,39 @@ function update() {
     $ClearCompleted.innerHTML= ' ';
       }
 
-// To remove the block of style if the array of items empty
+      // To remove the block of style if the array of items empty
       if (TODOS.length === 0) {
         document.querySelector('.main').style.display = 'none';
       } else {
         document.querySelector('.main').style.display = 'block';
+        }   
+
+//********//In update, create a filteredItems variable that contains either all items, only the active items or the completed items.
+
+        filteredItems = TODOS;
+
+    }//End Update************
+
+
+//********//  Use the filter function for that (specifically, use it three times,
+//******** // one in each branch of the if condition)
+    
+      function onChangeFilter(Filtersno){
+        console.log(" Event is " + Filtersno);
+        if (Filtersno ==="completed"){
+          CompletedTodo = TODOS.filter(items => items.done );
+                  console.log("completed filteredItems  " + CompletedTodo);
+        } else if (Filtersno ==="active"){
+            ActiveTodo = TODOS.filter( items =>!items.done);
+          console.log("active filteredItems  " + ActiveTodo);
+          document.querySelector(".main").style.display = "block";
         }
-      }
+               else {
+          filteredItems = TODOS;
+          console.log("All filteredItems  " + filteredItems);
+        }
+       update();
+        }   
        
   function onToggleTodo(id) {
     const todo = TODOS.find(todo => todo.id === id);
@@ -105,3 +132,14 @@ function update() {
 
   const $clearCompleted = document.querySelector(".clear-completed");
   $clearCompleted.addEventListener("click", ClearCompleted);
+
+  //********//Add three event listeners for our three buttons. You can use bind to connect the three buttons to the same onChangeFilter button, 
+//********//but you can also just make three filters. 
+  const $allTodo = document.querySelector('.filters').childNodes;
+  $allTodo[1].addEventListener("click",onChangeFilter.bind(null,"all"));
+  $allTodo[3].addEventListener("click",onChangeFilter.bind(null,"active"));
+  $allTodo[5].addEventListener("click",onChangeFilter.bind(null,"completed"));
+  console.log ($allTodo[1]);
+  console.log ($allTodo[3]);
+  console.log ($allTodo[5]);
+
